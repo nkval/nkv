6aa19733a828868cc69b292965d5523fad2155d2
Pavel Abramov <uncle.decart@gmail.com>
2025-02-13 12:19:14 +0100

    Add flags parsing to nkv-server and nkv-client
    So that user can specify socket to connect to and
    directory for nkv-server where it will store files.
    Instead of just specifying them in orderly manner I decided to
    use flags, because it is more straightforward approach and it is backward
    and forward-compatible friendly. In order to keep binary size as small as
    possible, instead of brining a ready library which would increase size for 30-50Kbytes,
    I wrote simple FlagParser, which does it's job and don't bring any 3rd party dependecies,
    apart from HashMap and HashSet from collections, which were already part of nkv.
    
    Signed-off-by: Pavel Abramov <uncle.decart@gmail.com>

--- src/server/main.rs
+++ src/server/main.rs
@@ -1,29 +1,53 @@
-use std::{env, fs};
+use std::{env, fs, path::PathBuf};
 use tempfile::TempDir;
 
+use nkv::flag_parser::FlagParser;
 use nkv::srv;
 
 const DEFAULT_URL: &str = "/tmp/nkv/nkv.sock";
 
+const HELP_MESSAGE: &str = "nkv-server [--dir path-to-store-data] [--sock path-to-socket] [--help]\
+    run notify key value server\
+    --dir - specify where to store files, if not specified it will create temprorary directory\
+    --sock - specify where to create UNIX socket to listen to connections to\
+    --help - display this message";
+
 #[tokio::main(flavor = "multi_thread")]
 async fn main() {
+    let allowed_flags = vec!["dir".to_string(), "help".to_string(), "sock".to_string()];
     let args: Vec<String> = env::args().collect();
-
-    let url = if args.len() > 1 {
-        &args[1]
-    } else {
-        DEFAULT_URL
+    let flags = match FlagParser::new(args, Some(allowed_flags)) {
+        Ok(res) => res,
+        Err(err) => {
+            println!("error: {}", err);
+            println!("{}", HELP_MESSAGE);
+            return;
+        }
     };
-    if fs::metadata(url).is_ok() {
-        fs::remove_file(url).expect("Failed to remove old socket");
+
+    if flags.get("help").is_some() {
+        println!("{}", HELP_MESSAGE);
+        return;
     }
 
-    let temp_dir = TempDir::new().expect("Failed to create temporary directory");
+    let sock_path = match flags.get("sock") {
+        Some(&Some(ref val)) => val.clone(),
+        _ => DEFAULT_URL.to_string(),
+    };
+
+    if fs::metadata(&sock_path).is_ok() {
+        fs::remove_file(&sock_path).expect("Failed to remove old socket");
+    }
+
+    let dir = match flags.get("dir") {
+        Some(&Some(ref val)) => PathBuf::from(val),
+        _ => TempDir::new()
+            .expect("Failed to create temporary directory")
+            .into_path(),
+    };
 
     // creates a task where it waits to serve threads
-    let (mut srv, _cancel) = srv::Server::new(url.to_string(), temp_dir.path().to_path_buf())
-        .await
-        .unwrap();
+    let (mut srv, _cancel) = srv::Server::new(sock_path.to_string(), dir).await.unwrap();
 
     srv.serve().await;
 }
